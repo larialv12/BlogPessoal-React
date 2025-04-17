22b95300270ba36fd77ff2dd84a8e8b4d2295213
Larissa Alves <larialves20015@gmail.com>
2025-04-16 21:38:20 -0300

    estilizacao home

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/home/Home'
+import Navbar from './componentes/Navbar/Navbar'
+import Footer from './componentes/Footer/Footer'
+
 
 
 function App() {
 
   return (
     <div>
-
+     <Navbar />
       <Home />
-
+     <Footer/>
 
     </div>
 
