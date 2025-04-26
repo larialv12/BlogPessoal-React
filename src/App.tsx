bf1d0e33070e0852be57081b5b0ad809b80b7072
Larissa Alves <larialves20015@gmail.com>
2025-04-26 00:23:53 -0300

    autenticacao cadastro e login

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
 import Footer from './componentes/Footer/Footer'
 import Navbar from './componentes/Navbar/Navbar'
 import Home from './pages/home/Home'
-import './App.css'
+import Login from './pages/login/Login'
+import Cadastro from './pages/cadastro/Cadastro'
+import { AuthProvider } from './contexts/AuthContext'
+import ListaTemas from './componentes/Temas/listatemas/ListaTemas'
 
 function App() {
   return (
     <>
+    <AuthProvider>
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/temas" element={<ListaTemas />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+        </AuthProvider>
     </>
   )
 }
 
 export default App
+
